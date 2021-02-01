904cb6956e5c73fe3b25286c524fe3c147aac54e
ae22567 <59934169+ae22567@users.noreply.github.com>
2021-02-01 21:15:00 +0800

    created project

---
+++ Assets/collection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class collection : MonoBehaviour
+{
+
+    public AudioSource tickSource;
+
+    void Start()
+    {
+        tickSource = GetComponent<AudioSource>();
+    }
+        void OnTriggerEnter2D(Collider2D col)
+    {
+        tickSource.Play();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/scripts/Character2dController.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class Character2dController : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+    }
+}
